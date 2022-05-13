e54ba577c76eefeada33068c0244e6e131b364b6
Italo MAGESTE <104988857+italommf@users.noreply.github.com>
2022-05-13 13:29:29 +0000

    .

--- Aulas/Aula de Metodos.cs
+++ Aulas/Aula de Metodos.cs
@@ -1,16 +1,18 @@
 using System;
 class Program{
   public static void Main(){
-    int x = MenorValor(4, 5, 2);
-    Console.WriteLine(x);
-    Console.WriteLine(MenorValor(8, 1, 5));
-    int y = MenorValor(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+    //int x = MenorValor(4, 5, 2);
+    //Console.WriteLine(x);
+    //Console.WriteLine(MenorValor(8, 1, 5));
+    int y = MenorMaiorValor(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
     Console.WriteLine(y);
   }
-  public static int MenorValor(int a, int b, int c){
+  public static int MenorMaiorValor(int a, int b, int c, out int menor, out int maior){
     int menor = a;
     if(b < menor) menor = b;
     if(c < menor) menor = c;
-    return menor;
-  }
+    int maior = a;
+    if(b > maior) maior = b;
+    if(c > maior) maior = c;
+    }
 }
